4d3f7beb22040c426436a64479d225b850da4800
Fabrice Moll <fabrice.moll@hotmail.com>
2015-08-22 10:17:49 +0200

    MemberService Test Class

---
+++ BetaSeries.API.Tests/MemberServiceTest.cs
@@ -0,0 +1,58 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace BetaSeries.API.Tests
+{
+	[TestClass]
+	public class MemberServiceTest
+	{
+		private MemberService _memberService;
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			_memberService = new MemberService
+			{
+				Token = TestSettings.Token,
+				ApiKey = TestSettings.ApiKey,
+				UserAgent = TestSettings.UserAgent
+			};
+		}
+
+		[TestMethod]
+		public async Task AuthASync()
+		{
+			var rootAuth = await _memberService.AuthASync("dev029", "developer");
+
+			Assert.IsTrue(!string.IsNullOrEmpty(rootAuth.Token));
+		}
+
+		[TestMethod]
+		public async Task DisconnectASync()
+		{
+			var rootAuth = await _memberService.AuthASync("dev029", "developer");
+
+			Assert.IsTrue(!string.IsNullOrEmpty(rootAuth.Token));
+
+			var result = await _memberService.DisconnectASync(rootAuth.Token);
+
+			Assert.IsTrue(result);
+		}
+
+		[TestMethod]
+		public async Task InfosASync()
+		{
+			var result = await _memberService.InfosASync();
+
+			Assert.IsNotNull(result);
+		}
+
+		[TestMethod]
+		public async Task FindBadgesASync()
+		{
+			var result = await _memberService.FindBadgesASync(86198);
+
+			Assert.IsNotNull(result);
+		}
+	}
+}
