5ba9452044b0783c7faa112d149b30f016849cd9
Fabrice Moll <fabrice.moll@hotmail.com>
2015-09-01 19:51:46 +0200

    Added MessageService

---
+++ BetaSeries.API/MessageService.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BetaSeries.API.Extensions;
+using BetaSeries.API.Model;
+
+namespace BetaSeries.API
+{
+	public interface IMessageService
+	{
+		Task<IList<Message>> FindMessagesASync(int page = 0);
+		Task<IList<Message>> GetDiscussionASync(int id);
+		Task<Message> DeleteASync(int id);
+		Task<Message> SendASync(int toId, string message, string title, int parentMessageId);
+		Task<Message> MarkAsReadASync(int id);
+	}
+
+	public class MessageService : BaseService, IMessageService
+	{
+		private const string InboxUrl = "/messages/inbox";
+		private const string DiscussionUrl = "/messages/discussion";
+		private const string SendDeleteUrl = "/messages/message";
+		private const string ReadUrl = "/messages/read";
+
+		public async Task<IList<Message>> FindMessagesASync(int page = 0)
+		{
+			var postData = new Dictionary<string, string>();
+
+			if (page > 0)
+				postData.Add("page", page);
+
+			var options = "?" + postData.ToQueryString();
+
+			var response = await Get<MessageList>(InboxUrl + options);
+
+			ValidateResponse(response);
+
+			return response.Messages;
+
+		}
+
+		public async Task<IList<Message>> GetDiscussionASync(int id)
+		{
+			var postData = new Dictionary<string, string>();
+
+			postData.Add("id", id);
+
+			var options = "?" + postData.ToQueryString();
+
+			var response = await Get<MessageList>(DiscussionUrl + options);
+
+			ValidateResponse(response);
+
+			return response.Messages;
+		}
+
+		public async Task<Message> DeleteASync(int id)
+		{
+			var postData = new Dictionary<string, string>();
+
+			postData.Add("id", id);
+
+			var options = "?" + postData.ToQueryString();
+
+			var response = await Delete<RootMessage>(SendDeleteUrl + options);
+
+			ValidateResponse(response);
+
+			return response.Message;
+		}
+
+		public async Task<Message> SendASync(int toId, string message, string title, int parentMessageId)
+		{
+			var postData = new Dictionary<string, string>();
+
+			postData.Add("to", toId);
+
+			postData.Add("text", message);
+
+			postData.Add("title", title);
+
+			if (parentMessageId > 0)
+				postData.Add("id", parentMessageId);
+
+			var options = "?" + postData.ToQueryString();
+
+			var response = await Post<RootMessage>(SendDeleteUrl + options, null);
+
+			ValidateResponse(response);
+
+			return response.Message;
+		}
+
+		public async Task<Message> MarkAsReadASync(int id)
+		{
+			var postData = new Dictionary<string, string>();
+
+			postData.Add("id", id);
+
+			var options = "?" + postData.ToQueryString();
+
+			var response = await Post<RootMessage>(ReadUrl + options, null);
+
+			ValidateResponse(response);
+
+			return response.Message;
+		}
+	}
+}
---
+++ BetaSeries.API/Model/Message.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using BetaSeries.API.Converters;
+using Newtonsoft.Json;
+
+namespace BetaSeries.API.Model
+{
+	public class Sender
+	{
+		[JsonProperty("id")]
+		public int Id { get; set; }
+
+		[JsonProperty("login")]
+		public string Login { get; set; }
+	}
+
+	public class Recipient
+	{
+		[JsonProperty("id")]
+		public int Id { get; set; }
+
+		[JsonProperty("login")]
+		public string Login { get; set; }
+	}
+
+	public class Message
+	{
+		[JsonProperty("id")]
+		public int Id { get; set; }
+
+		[JsonProperty("message_id")]
+		public int MessageId { get; set; }
+
+		[JsonProperty("inner_id")]
+		public int InnerId { get; set; }
+
+		[JsonProperty("sender")]
+		public Sender Sender { get; set; }
+
+		[JsonProperty("recipient")]
+		public Recipient Recipient { get; set; }
+
+		[JsonProperty("date")]
+		[JsonConverter(typeof(DateTimeConverterFromString))]
+		public DateTime Date { get; set; }
+
+		[JsonProperty("title")]
+        public string Title { get; set; }
+
+		[JsonProperty("text")]
+		public string Text { get; set; }
+
+		[JsonProperty("unread")]
+		public bool Unread { get; set; }
+
+		[JsonProperty("has_unread")]
+		public bool HasUnread { get; set; }
+	}
+
+	public class MessageList
+	{
+		[JsonProperty("messages")]
+		public List<Message> Messages { get; set; }
+
+		[JsonProperty("errors")]
+		public List<Error> Errors { get; set; }
+	}
+
+	public class RootMessage
+	{
+		[JsonProperty("message")]
+		public Message Message { get; set; }
+
+		[JsonProperty("errors")]
+		public List<Error> Errors { get; set; }
+	}
+}
