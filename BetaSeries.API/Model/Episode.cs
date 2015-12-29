c36ace284f3770a3bb19e2b01e88e1e444c844b2
Fabrice Moll <f.moll@easi.net>
2015-12-29 16:43:15 +0100

    Added PlanningService

--- BetaSeries.API/Model/Episode.cs
+++ BetaSeries.API/Model/Episode.cs
@@ -5,85 +5,94 @@ using Newtonsoft.Json;
 
 namespace BetaSeries.API.Model
 {
-	public class Episode
-	{
-		[JsonProperty("id")]
-		public int Id { get; set; }
+    public class Episode
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
 
-		[JsonProperty("thetvdb_id")]
-		public int ThetvdbId { get; set; }
+        [JsonProperty("thetvdb_id")]
+        public int ThetvdbId { get; set; }
 
-		[JsonProperty("youtube_id")]
-		public object YoutubeId { get; set; }
+        [JsonProperty("youtube_id")]
+        public object YoutubeId { get; set; }
 
-		[JsonProperty("title")]
-		public string Title { get; set; }
+        [JsonProperty("title")]
+        public string Title { get; set; }
 
-		[JsonProperty("season")]
-		public int SeasonNumber { get; set; }
+        [JsonProperty("season")]
+        public int SeasonNumber { get; set; }
 
-		[JsonProperty("episode")]
-		public int EpisodeNumber { get; set; }
+        [JsonProperty("episode")]
+        public int EpisodeNumber { get; set; }
 
-		[JsonProperty("show")]
-		public ShowInformation ShowInformation { get; set; }
+        [JsonProperty("show")]
+        public ShowInformation ShowInformation { get; set; }
 
-		[JsonProperty("code")]
-		public string Code { get; set; }
+        [JsonProperty("code")]
+        public string Code { get; set; }
 
-		[JsonProperty("global")]
-		public int Global { get; set; }
+        [JsonProperty("global")]
+        public int Global { get; set; }
 
-		[JsonProperty("description")]
-		public string Description { get; set; }
+        [JsonProperty("description")]
+        public string Description { get; set; }
 
-		[JsonProperty("date")]
-		[JsonConverter(typeof(DateTimeConverterFromString))]
-		public DateTime? Date { get; set; }
+        [JsonProperty("date")]
+        [JsonConverter(typeof(DateTimeConverterFromString))]
+        public DateTime? Date { get; set; }
 
-		[JsonProperty("note")]
-		public Note Note { get; set; }
+        [JsonProperty("note")]
+        public Note Note { get; set; }
 
-		[JsonProperty("user")]
-		public UserInformation UserInformation { get; set; }
+        [JsonProperty("user")]
+        public UserInformation UserInformation { get; set; }
 
-		[JsonProperty("comments")]
-		public string Comments { get; set; }
+        [JsonProperty("comments")]
+        public string Comments { get; set; }
 
-		[JsonProperty("subtitles")]
-		public List<SubTitle> Subtitles { get; set; }
-	}
+        [JsonProperty("subtitles")]
+        public List<SubTitle> Subtitles { get; set; }
+    }
 
-	public class RootEpisode
-	{
-		[JsonProperty("episode")]
-		public Episode Episode { get; set; }
+    public class RootEpisode
+    {
+        [JsonProperty("episode")]
+        public Episode Episode { get; set; }
 
-		[JsonProperty("note")]
-		public int Note { get; set; }
+        [JsonProperty("note")]
+        public int Note { get; set; }
 
-		[JsonProperty("errors")]
-		public List<Error> Errors { get; set; }
-	}
+        [JsonProperty("errors")]
+        public List<Error> Errors { get; set; }
+    }
 
-	public class ShowInformation
-	{
-		[JsonProperty("id")]
-		public int Id { get; set; }
+    public class ShowInformation
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
 
-		[JsonProperty("thetvdb_id")]
-		public int ThetvdbId { get; set; }
+        [JsonProperty("thetvdb_id")]
+        public int ThetvdbId { get; set; }
 
-		[JsonProperty("title")]
-		public string Title { get; set; }
-	}
+        [JsonProperty("title")]
+        public string Title { get; set; }
+    }
 
-	public class UserInformation
-	{
-		[JsonProperty("seen")]
-		public bool Seen { get; set; }
+    public class UserInformation
+    {
+        [JsonProperty("seen")]
+        public bool Seen { get; set; }
 
-		[JsonProperty("downloaded")]
-		public bool Downloaded { get; set; }
-	}
+        [JsonProperty("downloaded")]
+        public bool Downloaded { get; set; }
+    }
+
+    public class EpisodeList
+    {
+        [JsonProperty("episodes")]
+        public List<Episode> Episodes { get; set; }
+
+        [JsonProperty("errors")]
+        public List<Error> Errors { get; set; }
+    }
 }
---
+++ BetaSeries.API/PlanningService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BetaSeries.API.Extensions;
+using BetaSeries.API.Model;
+
+namespace BetaSeries.API
+{
+    public interface IPlanningService
+    {
+        Task<IList<Episode>> GetForMemberAsync(string token, bool onlyUnseen, DateTime? monthPlanning);
+        Task<IList<Episode>> GetForMemberAsync(int userId, bool onlyUnseen, DateTime? monthPlanning);
+    }
+
+    public class PlanningService : BaseService, IPlanningService
+    {
+        private const string MemberUrl = "/planning/member";
+
+
+        public async Task<IList<Episode>> GetForMemberAsync(string token, bool onlyUnseen, DateTime? monthPlanning)
+        {
+            var postData = new Dictionary<string, string>();
+
+            postData.Add("token", token);
+
+            if (onlyUnseen)
+                postData.Add("unseen", "1");
+
+            if (monthPlanning != null)
+                postData.Add("month", monthPlanning.Value.ToString("yyyy-MM"));
+
+
+            var options = "?" + postData.ToQueryString();
+
+
+            var response = await Get<EpisodeList>(MemberUrl + options);
+
+            ValidateResponse(response);
+
+            return response.Episodes;
+        }
+
+        public async Task<IList<Episode>> GetForMemberAsync(int userId, bool onlyUnseen, DateTime? monthPlanning)
+        {
+            var postData = new Dictionary<string, string>();
+
+            postData.Add("id", userId);
+
+            if (onlyUnseen)
+                postData.Add("unseen", "1");
+
+            if (monthPlanning != null)
+                postData.Add("month", monthPlanning.Value.ToString("yyyy-MM"));
+
+
+            var options = "?" + postData.ToQueryString();
+
+
+            var response = await Get<EpisodeList>(MemberUrl + options);
+
+            ValidateResponse(response);
+
+            return response.Episodes;
+        }
+    }
+}
