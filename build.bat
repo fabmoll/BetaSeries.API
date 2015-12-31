fe22a6a0ea23176614b443931fc56c4e62ede5d9
Fabrice Moll <f.moll@easi.net>
2015-12-31 12:13:33 +0100

    Build.Bat and nuspec file

--- build.bat
+++ build.bat
@@ -17,6 +17,6 @@ REM Build Solution
 
 REM Package
 mkdir Build
-nuget pack "BetaSeries.API\BetaSeries.API.csproj" -symbols -o Build -p Configuration=%config% %version%
+nuget pack BetaSeries.API.nuspec
 copy BetaSeries.API\bin\%config%\*.dll Build
 copy BetaSeries.API\bin\%config%\*.pdb Build
