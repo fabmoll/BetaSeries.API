09fefad93ac8418c0671d6c3072c12147412cd8f
Fabrice Moll <fabrice.moll@hotmail.com>
2015-12-31 17:48:25 +0100

    nuspec

--- build.bat
+++ build.bat
@@ -18,6 +18,6 @@ REM Build Solution
 REM Package
 mkdir Build
 mkdir Build\lib
-nuget pack BetaSeries.API.nuspec -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+nuget pack "BetaSeries.API.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
 copy BetaSeries.API\bin\%config%\*.dll Build\lib
 copy BetaSeries.API\bin\%config%\*.pdb Build\lib
