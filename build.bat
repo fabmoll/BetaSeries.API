1b9a830cda6b44763b6ed61c43975ae3c288ad8d
Fabrice Moll <f.moll@easi.net>
2015-12-31 11:50:16 +0100

    Build.Bat and nuspec file

--- build.bat
+++ build.bat
@@ -1,4 +1,22 @@
+@echo Off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+
+set version=
+if not "%PackageVersion%" == "" (
+   set version=-Version %PackageVersion%
+)
+
+REM Package Restore
 nuget restore BetaSeries.API.sln
-"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild" BetaSeries.API.sln
-nuget install Kulman.WPA81.BaseRestService
-nuget install Newtonsoft.Json
+
+REM Build Solution
+"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild" BetaSeries.API.sln /p:Configuration="%config%"
+
+REM Package
+mkdir Build
+nuget pack "BetaSeries.API\BetaSeries.API.csproj" -symbols -o Build -p Configuration=%config% %version%
+copy BetaSeries.API\bin\%config%\*.dll Build
+copy BetaSeries.API\bin\%config%\*.pdb Build
